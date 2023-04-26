8f3f5e5a407cfddf5ce5543fddcaac089e84bc79
NiNejah <almawlanejeh@gmail.com>
2023-04-26 13:52:27 +0200

    add : toolbarAwt & make RectangleAwt as Jpanel

--- xshapeV3.0/src/main/java/xshape/App.java
+++ xshapeV3.0/src/main/java/xshape/App.java
@@ -7,8 +7,8 @@ import javafx.stage.Stage;
 public class App {
 
     public static void main(String[] args) {
-        //XShape appAwt = new AwtApp();
-        //appAwt.run();
-        FxApplication.main(null);
+        XShape appAwt = new AwtApp();
+        appAwt.run();
+//        FxApplication.main(null);
     }
 }
